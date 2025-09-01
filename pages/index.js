1ac42e706b13caf747e8f10e40e32ac8522ac02e
tikutikukotoba <tikutikukotoba12090616@gmail.com>
2025-09-01 12:04:04 +0900

    Update title for preview test

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>{sitetitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
